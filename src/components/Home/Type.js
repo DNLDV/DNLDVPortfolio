48edd380d5b831be6ccce7c87f0536624fdfcf50
May-xxvi98 <shielamaydevega98@gmail.com>
2025-07-28 15:13:22 +0800

    Update Code

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
+          "Front-End Developer",
           "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Data Incoder",
+          
         ],
         autoStart: true,
         loop: true,
