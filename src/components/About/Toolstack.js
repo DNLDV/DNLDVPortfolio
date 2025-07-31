5b88c46e5dc1325c994b63d06504c113a8f254b5
May-xxvi98 <shielamaydevega98@gmail.com>
2025-08-01 00:15:40 +0800

    new

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,12 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import {
-  SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
-  SiVercel,
-  SiMacos,
-} from "react-icons/si";
+import { SiVisualstudiocode, SiMacos } from "react-icons/si";
 
 function Toolstack() {
   return (
@@ -16,15 +10,6 @@ function Toolstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
       </Col>
     </Row>
   );
